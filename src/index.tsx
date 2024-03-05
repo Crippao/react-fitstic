c964d5d75ef0a6c1ac94ce4aae8fb6fce299a1e6
Crippao <cristina.paoloni@outlook.com>
2024-03-05 09:51:00 +0100

    aggiunta ripasso typescript

--- src/index.tsx
+++ src/index.tsx
@@ -11,129 +11,81 @@
 //   </React.StrictMode>
 // );
 
-export { }
+export{}
 
-console.log("hello!");
-
-// Tipi primitivi
-const i: number = 0;
-let str: string = "hello!";
-let bool: boolean = true;
-str = "world!";
-//let array: number[] = [1,2,"ciao", true];
-let array: number[] = [1, 2, 3, 4];
-
-// Enum
 enum WeekDays {
-  MONDAY,
+  MONDAY, 
   TUESDAY,
   WEDNESDAY
 }
-let myDay: WeekDays = WeekDays.MONDAY;
-let dayArray: WeekDays[] = [
-  WeekDays.MONDAY,
-  WeekDays.WEDNESDAY
-]
 
-// Operazioni con gli array
-console.log(array);
-array.push(5);
-console.log(array);
-
-let nuovoArray = array.concat(6);
-console.log("array", array);
-console.log("nuovoArray", nuovoArray);
-
-const arrayImmutabile = [1, 2, 3];
-arrayImmutabile.push(4);
-
-// Oggetti e strutture dati
-let punto = {
-  x: 3, y: 7
-};
-
+let myday:WeekDays = WeekDays.MONDAY;
+let dayArray: WeekDays[] = [ WeekDays.MONDAY,WeekDays.TUESDAY, WeekDays.WEDNESDAY];
+console.log(myday)
+console.log(dayArray);
 
 interface Point {
-  readonly x: number;
-  readonly y: number;
-  readonly z?: number;
+  readonly x:number;
+  readonly y:number;
 }
 
-const mioPunto: Point = {
-  x: 5,
-  y: 6
-};
-//mioPunto.x = 10;
-interface Point3d extends Point {
-  z: number;
+interface Point3D extends Point {
+  z:number;
 }
-const punto3d: Point3d = {
-  x: 1,
-  y: 1,
-  z: 1
-};
-console.log(punto3d);
+const p : Point3D = {x:5,y:6,z:65}
+const p2: Point3D = p;
+console.log("p",p); //{x:5,y:6,z:65}
+console.log("p2",p2); //{x:5,y:6,z:65}
 
-const { z, y, x } = punto3d;
-console.log("x: ", x);
+p2.z = 55;
+console.log("p",p); //{x:5,y:6,z:55}
+console.log("p2",p2); //{x:5,y:6,z:55}
 
-const punto3d2: Point3d = { ...punto3d };
+const p3 :Point3D = {...p} //spread operator
+console.log("p3",p3);
 
-console.log("punti iniziali");
-console.log("punto3d", punto3d);
-console.log("punto3d2", punto3d2);
-punto3d2.z = 2;
-console.log("punto3d", punto3d);
-console.log("punto3d2", punto3d2);
+const {x,y,z} = p;
+console.log("x =",x)
+console.log("y =",y)
+console.log("z =",z)
 
-
-
-// Funzioni
-
-console.log(sum(2, 3));
-function sum(n1: number, n2: number): number {
-  return n1 + n2;
-}
-
+// interfaccia funzionale
 interface Sum {
-  (n1: number, n2: number): number;
+  (n1:number,n2:number) : number;
 }
 
-const sum2: Sum = (n1, n2) => n1 + n2;
-console.log(sum2(2, 3));
+const somma : Sum = (n1:number,n2:number) => {return n1+n2}
 
-interface PuntoConFunzione {
-  readonly x: number;
-  readonly y: number;
-  readonly z?: number;
-  readonly somma: Sum;
+interface PointWithSum {
+  readonly x:number;
+  readonly y:number;
+  readonly z?:number;
+  readonly sum:Sum;
 }
 
-const mioPuntoConFunzione: PuntoConFunzione = {
+const myPointSum :PointWithSum = {
   x: 2,
-  y: 4,
-  somma: (n1: number, n2: number) => {
-    return n1 + n2
-  }
+  y: 19,
+  sum: somma
 }
 
-mioPuntoConFunzione.somma(2, 3);
+console.log("MyPointSum.sum =",myPointSum.sum(myPointSum.x,myPointSum.y))
 
+//MAP
+const array1 = [1,2,3]
+const arrayDouble = array1.map((e) => e*2)
+console.log("array1 =",array1)
+console.log("arrayDouble =",arrayDouble)
 
+const array2 : number[] = null as any;
+//solo se array2 non è null faccio il map, poi se il primo non è null restituisco il risultato 
+//del map altrimenti restituisco il secondo risultato([])
+const arrayDouble2 = array2?.map((e) => e*2) ?? []; 
+console.log("array2 =",array2)
+console.log("arrayDouble2 =",arrayDouble2)
 
-// Funzioni classiche della programmazione funzionale
-
-const arr1: number[] = null as any;
-
-const risultato = arr1?.map(item => (item % 2) === 0) ?? [];
-
-
-console.log("arr1", arr1);
-console.log("risultato", risultato);
-
-
-
-const arr2 = [1, 2, 3];
-const risultato2 = arr2.filter(item => (item % 2) === 0);
-console.log("arr2", arr2);
-console.log("risultato2", risultato2);
+//FILTER
+const arr1 = [1,2,3,4]
+const arr2 = arr1.filter((e)=> (e%2) === 0)
+console.log("arr1 =",arr1)
+console.log("arr2 =",arr2)
